e4f7f5e6272443dc5eacc612ef564feb36e1b467
duikerddd <44222023+duikerddd@users.noreply.github.com>
2020-03-13 21:25:58 +0800

    Update decltype.cpp

--- decltype.cpp
+++ decltype.cpp
@@ -21,6 +21,7 @@ decltype(a + b) c;
 cout << typeid(c).name() << endl;
 
 
+
 template <class Tp>
 void print_ilist(const initializer_list<Tp> &ilist)
 {
@@ -45,38 +46,6 @@ void *GetMemory(size_t size)
 return malloc(size);
 }
 
-//本质上是先将共同部分完成,再对不同处调整
-class Info{
-public:
-Info() :_num(1), _c('a'){}
-
-Info(int num) :Info()
-{
-_num = num;
-}
-Info(char c) :Info(), _c(2)
-{
-_c = c;
-}
-int _num = 1;
-char _c = 'a';
-};
-
-
-int main()
-{
-Info t1;
-Info t2{ 2 };
-Info t3{ 'b' };
-cout << t1._c << " " << t1._num << endl;
-cout << t2._c << " " << t2._num << endl;
-cout << t3._c << " " << t3._num << endl;
-
-getchar();
-return 0;
-}
-*/
-/*
 
 //类内声明,类外定义显示缺省赋值运算符重载
 Point& Point::operator=(const Point& p) = default;
