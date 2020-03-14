deeaaa714ab41f1f27f08672c4fc8c7afbb38d4b
duikerddd <44222023+duikerddd@users.noreply.github.com>
2020-03-14 21:12:27 +0800

    Update decltype.cpp

--- decltype.cpp
+++ decltype.cpp
@@ -47,19 +47,3 @@ return malloc(size);
 }
 
 
-//类内声明,类外定义显示缺省赋值运算符重载
-Point& Point::operator=(const Point& p) = default;
-
-
-int main()
-{
-Point p1;
-Point p2(2, 2);
-p1.Print();
-p2.Print();
-
-getchar();
-return 0;
-}
-
-
