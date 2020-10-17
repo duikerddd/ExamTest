82d343a342084d01546300e3484d7d7e6adfbb42
duikerddd <44222023+duikerddd@users.noreply.github.com>
2020-10-17 12:22:52 +0800

    Create my_shared_ptr.cpp

---
+++ my_shared_ptr.cpp
@@ -0,0 +1,116 @@
+#include<iostream>
+#include<mutex>
+#include<string>
+using namespace std;
+
+
+template<class T>
+class SharedPtr{
+public:
+	//构造 --- 初始化资源,++操作.和计数
+	SharedPtr(T* ptr) 
+	  :	_ptr(ptr),
+		_cnt(new int(1)),
+		_mtx(new mutex)
+	{
+			if (_ptr == nullptr)
+				*_cnt = 0;
+	}
+
+	//析构 --- --,为0释放资源
+	~SharedPtr()
+	{
+		Realase();
+	}
+
+	//拷贝构造 --- ++操作
+	SharedPtr(const SharedPtr<T>& tmp)
+		: _ptr(tmp._ptr),
+		  _cnt(tmp._cnt),
+		  _mtx(tmp._mtx)
+	{
+		if (_ptr)
+			AddRefCount();
+	}
+
+	//赋值
+	SharedPtr<T>& operator=(const SharedPtr<T>& tmp)
+	{
+		_ptr = tmp._ptr;
+		_cnt = tmp._cnt;
+		_mtx = tmp._mtx;
+
+		if (_ptr)
+			AddRefCount();
+	}
+
+	T& operator*()
+	{
+		return *_ptr;
+	}
+
+	T* operator->()
+	{
+		return _ptr;
+	}
+
+	int Count()
+	{
+		return *_cnt;
+	}
+
+	T* Get()
+	{
+		return _ptr;
+	}
+	
+	
+
+private:
+	T* _ptr;
+	int* _cnt;
+	mutex* _mtx;
+
+	//引用计数 ++
+	int AddRefCount()
+	{
+		_mtx->lock();
+		(*_cnt)++;
+		_mtx->unlock();
+		return *_cnt;
+	}
+
+	//--
+	int SubRefCount()
+	{
+		_mtx->lock();
+		(*_cnt)--;
+		_mtx->unlock();
+		return *_cnt;
+	}
+
+	void Realase()
+	{
+		if (_ptr && SubRefCount() == 0)
+		{
+			delete _ptr;
+			delete _cnt;
+		}
+	}
+};
+
+int main()
+{
+	int* n = new int(2);
+	
+	{
+		SharedPtr<int> sp(n);
+		cout << sp.Count() << endl;
+
+		SharedPtr<int> sp2(sp);
+		cout << sp.Count() << endl;
+	}
+	cout << *n << endl;
+
+	return 0;
+}
